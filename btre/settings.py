7b93b71f32c54a8454878e44d76b23798aa318fe
Sharmassshivam <shivamsharmass1234@gmail.com>
2021-05-28 20:38:57 +0530

    added accounts app and register and login functionality

--- btre/settings.py
+++ btre/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'pages',
     'listings',
-    'realtors'
+    'realtors',
+    'accounts'
 ]
 
 MIDDLEWARE = [
@@ -126,6 +127,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'btre/static')
 ]
+#media folder settings
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+MEDIA_URL='/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from listings.models import Listings
+from realtors.models import Realtor
 
 # Create your views here.
 def index(request):
-    return  render(request,'pages/index.html')
+    list=Listings.objects.order_by('-list_date').filter(is_published=True)[:3]
+    content={
+        'liste':list
+    }
+    return  render(request,'pages/index.html',content)
 def about(request):
-    return render(request,'pages/About.html')
+     realtor=Realtor.objects.all()
+     mvp=Realtor.objects.all().filter(is_mvp=True)
+     content={
+         'lists':realtor,
+         'ismvp':mvp
+     }
+
+     return render(request,'pages/About.html',content)
