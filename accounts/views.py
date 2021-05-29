02b0029d9dee21b8695bcf9202ed924156fcbca8
Sharmassshivam <shivamsharmass1234@gmail.com>
2021-05-29 15:48:19 +0530

    changes in login and html pages

--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,12 @@ from django.shortcuts import render,redirect
 
 # Create your views here.
 def register(request):
-    return render(request,'accounts/register.html')
+    if request.method== "POST":
+        print("REGISTERED")
+        return redirect('register')
+    else:
+
+        return render(request,'accounts/register.html')
 def login(request):
     return render(request,'accounts/login.html')
 def logout(request):
