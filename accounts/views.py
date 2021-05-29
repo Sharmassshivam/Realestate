b23c307373a5873379924fcccbcc2bedf302655c
Sharmassshivam <shivamsharmass1234@gmail.com>
2021-05-29 19:38:18 +0530

    added login function and redirecting to dashboard

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.models import User
+from django.contrib import auth
 
 # Create your views here.
 def register(request):
@@ -23,7 +24,9 @@ def register(request):
                   else:
                       user=User.objects.create_user(username=username,password=password,email=email,first_name=first_name,last_name=last_name)
                       print("user created")
-                      return redirect('index')
+                      auth.login(request,user)
+                      user.save()
+                      return redirect('login')
 
 
          else:
@@ -37,6 +40,18 @@ def register(request):
 
         return render(request,'accounts/register.html')
 def login(request):
+    if request.method=='POST':
+        username=request.POST['username']
+        password=request.POST['password']
+
+        user=auth.authenticate(username=username,password=password)
+
+        if user is not None:
+            auth.login(request,user)
+            return redirect('dashboard')
+    else:
+        return  render(request,'accounts/login.html')
+
     return render(request,'accounts/login.html')
 def logout(request):
     return  redirect(index)
